9e010d0ce76929f31d950708250d0fac812098a0
Abdo <abdoashrafammar370@gmail.com>
2025-10-28 16:25:34 +0300

    appbar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:graduation_project/screens/home_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,9 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
 
-      home: Placeholder(),
+      home: HomeScreen(),
     );
   }
 }
