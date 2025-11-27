7df96bf38393a48b2041695c5677cae4c0df072d
michael <mw6873190@gmail.com>
2025-11-27 14:43:22 +0200

    home_page

--- lib/features/ui/screens/home_screen.dart
+++ lib/features/ui/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import '../../../core/utils/app_colors.dart';
 import '../../../core/utils/app_styles.dart';
