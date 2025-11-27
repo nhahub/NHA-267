7efbd4d6e37b3fbe69bbdee58bb7533dc43a8c23
michael <mw6873190@gmail.com>
2025-11-27 14:17:47 +0200

    Fixed Login/Register and added Headers

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:depi_graduation_project/core/utils/my_bloc_observer.dart';
 import 'package:depi_graduation_project/features/ui/auth/register/register_screen.dart';
+//import 'package:depi_graduation_project/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,11 +12,10 @@ import 'core/utils/app_routes.dart';
 import 'core/utils/app_theme.dart';
 import 'core/utils/theme_manager.dart';
 
-import 'features/ui/screens/splash_screen.dart';
-
 // (إمبورت شاشة تسجيل الدخول)
 import 'features/ui/auth/Login/login_screen.dart';
-import 'features/ui/pages/home_screen/home_screen.dart';
+import 'features/ui/pages/home_screen/home_screen.dart' as full_home;
+import 'features/ui/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,6 +24,7 @@ void main() async {
 
   await SharedPreferenceUtils.init();
   await ThemeManager.init();
+
 
   configureDependencies();
 
@@ -40,15 +41,18 @@ class MyApp extends StatelessWidget {
       minTextAdapt: true,
       splitScreenMode: true,
       builder: (context, child) {
+
         return ValueListenableBuilder<bool>(
           valueListenable: ThemeManager.isDark,
           builder: (context, isDark, _) {
             return MaterialApp(
               debugShowCheckedModeBanner: false,
               title: 'دليلك للإيجار',
+
               theme: AppTheme.lightTheme,
               darkTheme: AppTheme.darkTheme,
               themeMode: isDark ? ThemeMode.dark : ThemeMode.light,
+
               locale: const Locale('ar'),
               localizationsDelegates: const [
                 GlobalMaterialLocalizations.delegate,
@@ -59,17 +63,20 @@ class MyApp extends StatelessWidget {
                 Locale('en'),
                 Locale('ar'),
               ],
+
               builder: (context, widget) {
                 return Directionality(
                   textDirection: TextDirection.rtl,
                   child: widget!,
                 );
               },
+
               home: const SplashScreen(),
+
               routes: {
                 AppRoutes.loginRoute: (context) => const LoginScreen(),
                 AppRoutes.registerRoute: (context) => const RegisterScreen(),
-                AppRoutes.homeRoute:(context) => const HomeScreen(),
+                AppRoutes.homeRoute: (context) => const full_home.HomeScreen(),
               },
             );
           },
